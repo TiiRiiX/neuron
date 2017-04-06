91c910e1363ac1655021d0262b06aca60d1b415f
Manzick <manzurov.alex@yahoo.com>
2017-04-06 19:07:56 +0500

    All program

---
+++ Nuron/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace Nuron
+{
+    public class Neuron
+    {
+        public string name;
+        public int[,] input;
+        public int output;
+        public int[,] memory;
+
+        public Neuron(string name)
+        {
+            this.name = name;
+            this.input = new int[30, 30];
+            this.output = 0;
+            this.memory = new int[30, 30];
+            for (int i = 0; i < 30; i++)
+                for (int j = 0; j < 30; j++)
+                    memory[i, j] = 255;
+        }
+
+        /// <summary>
+        /// Сохранение памяти нейрона в картинку
+        /// </summary>
+        public void SaveMemory()
+        {
+            Bitmap memoryBmp = new Bitmap(30, 30);
+            for (int i = 0; i < 30; i++)
+                for (int j = 0; j < 30; j++) {
+                    Color color = Color.FromArgb(memory[i, j], memory[i, j], memory[i, j]);
+                    memoryBmp.SetPixel(i, j, color);
+                }
+            using (FileStream fs = new FileStream($"C:/Users/manzick/Project/Neuro/Memory/{name}.png", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                memoryBmp.Save(fs, ImageFormat.Png);
+        }
+
+        /// <summary>
+        /// Загружает память нейрона
+        /// </summary>
+        /// <returns>Смогла ли память загрузится</returns>
+        public bool LoadMemory()
+        {
+            try {
+                Bitmap memoryBmp;
+                using (FileStream fs = new FileStream($"C:/Users/manzick/Project/Neuro/Memory/{name}.png", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    memoryBmp = new Bitmap(fs);
+                for (int i = 0; i < 30; i++)
+                    for (int j = 0; j < 30; j++) {
+                        memory[j, i] = memoryBmp.GetPixel(j, i).R;
+                    }
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static Neuron[] Neuro_Web = new Neuron[10];
+        static Neuron maxNeo;
+        static int Success = 0;
+        static int Fall = 0;
+        public static List<string> ListOfAdress = new List<string>();
+
+        /// <summary>
+        /// Обновление списка файлов для распознания
+        /// </summary>
+        public static void RefreshFiles()
+        {
+            string[] files = Directory.GetFiles("C:/Users/manzick/Project/Neuro");
+            ListOfAdress.Clear();
+            for (int i = 0; i < files.Length; i++)
+                ListOfAdress.Add(files[i].Substring(files[i].IndexOf("Neuro") + 6));
+        }
+
+        /// <summary>
+        /// Править память неправильного нейрона
+        /// </summary>
+        /// <param name="filePath">Путь до файла распознавания</param>
+        /// <param name="nameNeuron">Имя исправляемого нейрона</param>
+        public static void CorrectMemory(string filePath, string nameNeuron)
+        {
+            Bitmap bmp = new Bitmap(filePath);
+            foreach (Neuron e in Neuro_Web)
+                if (e.name == nameNeuron) {
+                    for (int i = 0; i < 30; i++)
+                        for (int j = 0; j < 30; j++) {
+                            Color picPixel = bmp.GetPixel(j, i);
+                            if (picPixel.R < 250)
+                                e.memory[j, i] = Math.Abs(picPixel.R - e.memory[j, i]) / 2;
+                        }
+                    e.SaveMemory();
+                }
+        }
+
+        /// <summary>
+        /// Функция нейронного анализа картинки
+        /// </summary>
+        /// <param name="filePath">Путь до файла</param>
+        /// <returns>Нейрон с максимальным весом</returns>
+        public static Neuron NeuroAnalize(string filePath)
+        {
+            Bitmap bmp = new Bitmap(filePath);
+            Neuron maxNeuron = Neuro_Web[0];
+            foreach (Neuron neo in Neuro_Web) {
+                neo.output = 0;
+                for (int i = 0; i < 30; i++)
+                    for (int j = 0; j < 30; j++) {
+                        Color picPixel = bmp.GetPixel(j, i);
+                        if (Math.Abs(picPixel.R - neo.memory[j, i]) < 50 && (picPixel.R < 250 && picPixel.G < 250 && picPixel.B < 250)) {
+                            neo.output++;
+                        }
+                    }
+                if (maxNeuron.output < neo.output) {
+                    maxNeuron = neo;
+                }
+            }
+            return maxNeuron;
+        }
+
+        public static void CreateOrLoadWeb()
+        {
+            for (int i = 0; i < 10; i++) {
+                Neuro_Web[i] = new Neuron(i.ToString());
+                Neuro_Web[i].LoadMemory();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CreateOrLoadWeb();
+            RefreshFiles();
+            while (true) {
+                int choose = -1;
+                Console.WriteLine("Выберете файл для распознования:");
+                Console.WriteLine($"{choose++}) Выход");
+                foreach (string s in ListOfAdress)
+                    Console.WriteLine($"{choose++}) {s}");
+                Console.Write("Ввод: ");
+                choose = int.Parse(Console.ReadLine());
+                if (choose == -1) return;
+                Neuron newAnalize = NeuroAnalize($"C:/Users/manzick/Project/Neuro/{ListOfAdress[choose]}");
+                Console.WriteLine($"Сеть решила, что это {newAnalize.name}");
+                Console.WriteLine("Верно? (да +, нет -)");
+                string needToCorrect = Console.ReadLine();
+                if (needToCorrect == "-") {
+                    Console.WriteLine("Введите верный ответ:");
+                    string correctAnswer = Console.ReadLine();
+                    CorrectMemory($"C:/Users/manzick/Project/Neuro/{ListOfAdress[choose]}", correctAnswer);
+                } else
+                    CorrectMemory($"C:/Users/manzick/Project/Neuro/{ListOfAdress[choose]}", newAnalize.name);
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nuron;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static StreamWriter fs = new StreamWriter("C:/Users/manzick/Project/Neuro/Logs/log.txt");
+        static int Success = 0;
+        static int Fall = 0;
+
+        [TestMethod]
+        public void OneRandomNumber()
+        {
+            Random rand = new Random();
+            Program.RefreshFiles();
+            Program.CreateOrLoadWeb();
+            int setNum = rand.Next(Program.ListOfAdress.Count - 1);
+            int chooseNumber = (int)Char.GetNumericValue(Program.ListOfAdress[setNum][1]);
+            Neuron newAnalize = Program.NeuroAnalize($"C:/Users/manzick/Project/Neuro/{Program.ListOfAdress[setNum]}");
+            try {
+                Assert.AreEqual(newAnalize.name, chooseNumber.ToString());
+                Success++;
+                fs.WriteLine($"Success {Success}/{Success + Fall} {Program.ListOfAdress[setNum]}");
+                Program.CorrectMemory($"C:/Users/manzick/Project/Neuro/{Program.ListOfAdress[setNum]}", chooseNumber.ToString());
+            } catch {
+                Fall++;
+                fs.WriteLine($"Fail {Success}/{Success + Fall} {Program.ListOfAdress[setNum]}");
+                Program.CorrectMemory($"C:/Users/manzick/Project/Neuro/{Program.ListOfAdress[setNum]}", chooseNumber.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void HungredRandomNumber()
+        {
+            for (int i = 0; i < 100; i++)
+                OneRandomNumber();
+            fs.Close();
+        }
+    }
+}
